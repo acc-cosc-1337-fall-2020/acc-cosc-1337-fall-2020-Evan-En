a4eff7432f83276d3960062e23a352e3904d4e6d
evann <evan.niswanger@g.austincc.edu>
2020-08-31 18:43:51 -0500

    HW 2 works now

--- src/homework/02_expressions/main.cpp
+++ src/homework/02_expressions/main.cpp
@@ -1,15 +1,15 @@
 ﻿//write include statements
-
+#include "expressions.h"
 
 //write namespace using statement for cout
-
-
+#include<iostream>
+using namespace std;
 /*
 Call multiply_numbers with 10 and 10 parameter values and display function result
 */
 int main()
 {
-	
-
+	int num = multiply_numbers(10, 10);
+	std::cout<<num;
 	return 0;
 }

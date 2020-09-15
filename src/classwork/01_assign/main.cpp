a0759adde638fa1fc478100b6993edc12ffa9694
Art Gonzalez <arturo.gonzalez@austincc.edu>
2020-09-14 21:02:39 -0500

    hw 0 corrections

--- src/classwork/01_assign/main.cpp
+++ src/classwork/01_assign/main.cpp
@@ -2,20 +2,20 @@
 #include "types.h"
 //Write include for capturing input from keyboard and displaying output to screen
 #include<iostream>
-using namespace std;
+
+using std::cout; using std::cin; using std::endl;
 
 int main() 
 {
 	int num;
-	int num1;
+	int num1 = 4;
 	cout << "Enter a number: ";
 	cin >> num;
-	cout << "Enter a number: ";
-	cin >> num1;
-	int result = multiply_numbers(num, num1);
-	//cout << result << endl;
-	//int num1 = 4;
-	//result = multiply_numbers(num1);
+	
+	int result = multiply_numbers(num);
+	cout << result << endl;
+	
+	result = multiply_numbers(num1);
 	cout << "Result: " << result;
 	return 0;
 }

3958d6d2de0e94ca6f755cae5021df8374a85ea6
evann <evan.niswanger@g.austincc.edu>
2020-09-21 14:05:16 -0500

    Homework 2

--- src/classwork/03_assign/decision.cpp
+++ src/classwork/03_assign/decision.cpp
@@ -1,1 +1,37 @@
 //cpp
+char get_letter_grade_using_if(int grade)
+{
+    if(grade <= 100 && grade >= 90)
+    {
+        return 'A';
+    }
+    else if(grade <= 89 && grade >= 80)
+    {
+        return 'B';
+    }
+    else if(grade <= 79 && grade >= 70)
+    {
+        return 'C';
+    }
+    else if(grade <= 69 && grade >= 60)
+    {
+        return 'D';
+    }
+    else
+    {
+        return 'F';
+    }
+}
+char get_letter_grade_using_switch(int grade)
+{
+    grade = grade/10;
+    switch(grade)
+    {
+        case 10: return 'A';
+        case 9: return 'A';
+        case 8: return 'B';
+        case 7: return 'C';
+        case 6: return 'D';
+        default: return 'F';
+    }
+}
--- src/classwork/03_assign/main.cpp
+++ src/classwork/03_assign/main.cpp
@@ -1,10 +1,15 @@
-//Write the include statement for decisions.h here
-
+//Write the include statement for decision.h here
+#include "decision.h"
 //Write namespace using statements for cout and cin
-
+#include<iostream>
+using namespace std;
 int main() 
 {
-
+	int grade;
+	cout << "Enter your grade: ";
+	cin >> grade;
+	cout << "If Letter Grade: " << get_letter_grade_using_if(grade) << endl;
+	cout << "Switch Letter grade: " << get_letter_grade_using_switch(grade);
 	return 0;
 }
 

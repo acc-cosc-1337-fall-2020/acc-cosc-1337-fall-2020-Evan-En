a26304ddf5f58bdd8c7a7f21ed8fcb8124041fe8
evann <evan.niswanger@g.austincc.edu>
2020-10-05 19:52:29 -0500

    HW

--- src/homework/04_iteration/dna.cpp
+++ src/homework/04_iteration/dna.cpp
@@ -6,7 +6,18 @@ Calculate GC content:
 Iterate string count Gs and Cs, divide count by string length.
 Return quotient.
 */
-
+double get_gc_content(const std::string str)
+{
+    double gc = 0.0;
+    for(char c: str)
+    {
+        if(c == 'G' || c == 'C')
+        {
+            gc++;
+        }
+    }
+    return gc/str.size();
+}
 
 
 
@@ -14,7 +25,17 @@ Return quotient.
 Write code for function get_reverse_string that
 accepts a string parameter and returns a string reversed.
 */
-
+std::string get_reverse_string(std::string str)
+{
+    std::string ret = str;
+    int count = 0;
+    for(int i = str.size()-1; i >= 0; i--)
+    {
+        ret[count] = str[i];
+        count++;
+    }
+    return ret;
+}
 
 
 /*
@@ -27,4 +48,27 @@ b. iterate local string variable and
 c. return string
 
 */
+std::string get_dna_complement(std::string str)
+{
+    std::string comp = get_reverse_string(str);
+    for(int i = 0; i < comp.size(); i++)
+    {
+        switch(comp[i])
+        {
+            case 'A':
+                comp[i] = 'T';
+                break;
+            case 'T':
+                comp[i] = 'A';
+                break;
+            case 'C':
+                comp[i] = 'G';
+                break;
+            case 'G':
+                comp[i] = 'C';
+                break;
+        }
+    }
+    return comp;
+}
 
--- src/homework/04_iteration/main.cpp
+++ src/homework/04_iteration/main.cpp
@@ -1,8 +1,9 @@
 //write include statements
-
+#include<iostream>
+#include "dna.h"
 //write using statements
-
-
+using std::string;
+using std::cout;
 /*
 Write code that prompts user to enter 1 for Get GC Content, 
 or 2 for Get DNA Complement.  The program will prompt user for a 
@@ -12,5 +13,28 @@ user enters a y or Y.
 */
 int main() 
 {
+	int op = 0;
+	char con = 'y';
+	while(con == 'y' || con == 'Y')
+	{
+		cout<<"Enter 1 for GC content. Enter 2 for DNA complement."<<"\n";
+		std::cin>>op;
+		if(op == 1)
+		{
+			cout<<"Enter DNA"<<"\n";
+			string DNA;
+			std::cin>>DNA;
+			cout<<get_gc_content(DNA)<<"\n";
+		}
+		if(op == 2)
+		{
+			cout<<"Enter DNA"<<"\n";
+			string DNA;
+			std::cin>>DNA;
+			cout<<get_dna_complement(DNA)<<"\n";
+		}
+		cout<<"Enter y to continue. Enter n to stop."<<"\n";
+		std::cin>>con;
+	}
 	return 0;
 }

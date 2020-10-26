ae261736c77bb318d755d8face5dfa30c76e343d
Art Gonzalez <arturo.gonzalez@austincc.edu>
2020-10-26 15:22:35 -0500

    tictactoe 1 grading

--- test/homework_test/tic_tac_toe_test/tic_tac_toe_test.cpp
+++ test/homework_test/tic_tac_toe_test/tic_tac_toe_test.cpp
@@ -8,9 +8,11 @@ TEST_CASE("Check TicTacToe")
 {
 	TicTacToe tic;
 	tic.start_game("X");
-	for(int i = 1; i<10; i++)
+	for(int i = 1; i<9; i++)
 	{
 		tic.mark_board(i);
+		REQUIRE(tic.game_over() == false);
 	}
+	tic.mark_board(9);
 	REQUIRE(tic.game_over() == true);
 }
